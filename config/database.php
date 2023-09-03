3498bd4a85749a531b5ca14a9f060c195bcb40f6
Mauro <maurocurti1989@gmail.com>
2023-09-02 22:30:33 -0300

    q

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'dbs11737534'),
             'username' => env('DB_USERNAME', 'dbu1502741'),
-            'password' => env('DB_PASSWORD', '{falcon4!0}}'),
+            'password' => env('DB_PASSWORD', '{falcon4!0}'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
